c60d3c6797495a1df5166513f56a1808bf1fdc9e
ravens-saphiroth <Chengch@anystreaming.cn>
2018-03-30 14:12:42 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+html {
+  background-color: #0e3667;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+.el-select .el-input__inner {
+  width: 2.55rem;
+  height: .8rem;
+  font-size: .4rem;
+}
+.mt-range {
+  height: .5rem !important;
+  line-height: .5rem !important;
+}
+.parameter .mt-range{
+  width: 2.55rem;
+}
+.mt-range .mt-range-thumb {
+  border-radius: 0;
+  width: .6rem;
+  height: .4rem ;
+  top: 0.05rem;
+  /* background-color: #20a0ff; */
+  border-left: .3rem solid #20a0ff;
+  background-color: transparent;
+  box-shadow: none;
+}
+.mt-range .mt-range-content {
+  margin-right: .3rem;
+}
+.mt-range .mt-range-runway {
+  border-top-width: .08rem !important;
+  border-image-source: url('../static/bg_line.png');
+  border-image-width: 100% 100% 0% 0%;
+  border-image-repeat: round;
+  width: calc(100% + .3rem);
+}
+.mt-range .mt-range-progress {
+  background-color: #3a88bf;
+  height: .08rem !important;
+}
+
+.el-table .cell {
+  line-height: normal;
+}
+
+.system_content_css .mt-range .mt-range-runway {
+  width: calc(100% + .3rem);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <section>
+    <el-select v-model="value" @visible-change="handle" class="select-list" popper-class="option-list" placeholder="请选择">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+      </el-option>
+    </el-select>
+    <el-popover
+      ref="popover"
+      placement="top-start"
+      title="标题"
+      width="200"
+      trigger="click"
+      content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+    </el-popover>
+    <el-button v-popover:popover>click 激活</el-button>
+  </section>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        value: '',
+        options: [{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
+        }],
+        rangeValue: 10
+      }
+    },
+    methods: {
+      handle(visible) {
+        let docEl = document.querySelector('.select-list')
+        console.log(docEl, docEl.getBoundingClientRect())
+      },
+    }
+  }
+</script>
